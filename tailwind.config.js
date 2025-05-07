d4f6c54099b2766284cee230062e9b2bc7b7bac9
ebuka <ebukaopara180@gmail.com>
2025-05-07 12:01:59 +0100

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme: {
+        extend:{
+            gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+            outfit: ["Ovo", "serif"]
+        },
+        animation:{
+            spin_slow: 'spin 8s linear infinite'
+        },
+        colors:{
+            lightHover: '#fcf4ff',
+            darkHover: '#2a004a',
+            darkTheme: '#11001F'
+
+        },
+        boxShadow:{
+            'black': '4px 4px 0 #000',
+            'white': '4px 4px 0 #fff',
+        }
+        
+        }
+        
+    },
+    darkMode: 'selector'
+
+}
